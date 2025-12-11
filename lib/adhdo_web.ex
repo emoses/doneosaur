11865fa4d2fe53c4d76a99c1eaaba491c3fa2710
Evan Moses <evan@emoses.org>
2025-12-11 08:01:21 -0800

    PWA and icons

--- lib/adhdo_web.ex
+++ lib/adhdo_web.ex
@@ -17,7 +17,7 @@ defmodule AdhdoWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images audio favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images audio favicon.ico robots.txt manifest.json)
 
   def router do
     quote do
